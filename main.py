429080b479a57e9f30ed1cc9a85f45906b13b620
James Sharp <james.sharp544@protonmail.com>
2025-02-04 21:18:42 -0600

    Fix issues missed from refactor

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import StartggAPI
 import WriteCSV
 
-def api_test():
-    api = StartggAPI.StartggAPI()
-    request = api.build_tournament_request()
-    variables = api.build_tournament_variables(1735711200, 1704088800, "KY")
-    result = api.call_api(request, variables)
-    print(result)
-
 def main():
     api = StartggAPI.StartggAPI()
     request = api.build_tournament_request()
@@ -15,6 +8,6 @@ def main():
     result = api.call_api(request, variables)
     print(result)
 
-    ParseJSON.parse_json(result)
+    WriteCSV.dump_to_csv(result)
 
 main()
